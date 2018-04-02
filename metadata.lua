f1c6dcc9806aa5ae3cdd3929fba9d54886ddccaa
Aneurin Price <nprice@dizions.co.uk>
2018-04-02 20:22:57 +0100

    Initial Commit

---
+++ metadata.lua
@@ -0,0 +1,15 @@
+return PlaceObj('ModDef', {
+	'title', "Mod Config Demo",
+	'description', "A simple demo mod for modders to use as an example of how to integrate with [url=http://steamcommunity.com/sharedfiles/filedetails/?id=1340775972]Mod Config[/url].\r\n\r\nThis mod does nothing on its own, but adds several example options to the Mod Config dialogue.\r\n\r\nIf you're using the Lua Console mod, it will also log any config changes that are made using the dialogue, as an example of how to handle them.",
+	'tags', "",
+	'image', "ModConfigDemo.png",
+	'id', "f6JALLR",
+	'steam_id', "1343176152",
+	'author', "Waywocket",
+	'version', 7,
+	'lua_revision', 228184,
+	'code', {
+		"Code/Script.lua",
+	},
+	'saved', 1522363538,
+})
